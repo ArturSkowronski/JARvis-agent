8748d5bbd24c2857cbf22ce164b5bb39fd08e4ce
Artur Skowronski <me@arturskowronski.com>
2025-06-05 21:15:00 +0200

    Remove .vscode directory from git tracking

--- components/tools-panel.tsx
+++ components/tools-panel.tsx
@@ -9,32 +9,13 @@ import InputFileUpload from "./input-file-upload";
 
 export default function ContextPanel() {
   const {
-    fileSearchEnabled,
-    setFileSearchEnabled,
-    webSearchEnabled,
-    setWebSearchEnabled,
     functionsEnabled,
     setFunctionsEnabled,
   } = useToolsStore();
   return (
     <div className="h-full p-8 w-full bg-[#f9f9f9] rounded-t-xl md:rounded-none border-l-1 border-stone-100">
       <div className="flex flex-col overflow-y-scroll h-full">
-        <PanelConfig
-          title="File Search"
-          tooltip="Allows to search a knowledge base (vector store)"
-          enabled={fileSearchEnabled}
-          setEnabled={setFileSearchEnabled}
-        >
-          <FileSearchSetup />
-        </PanelConfig>
-        <PanelConfig
-          title="Web Search"
-          tooltip="Allows to search the web"
-          enabled={webSearchEnabled}
-          setEnabled={setWebSearchEnabled}
-        >
-          <WebSearchConfig />
-        </PanelConfig>
+    
         <PanelConfig
           title="Functions"
           tooltip="Allows to use locally defined functions"
